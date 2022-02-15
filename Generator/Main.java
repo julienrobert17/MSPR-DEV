a0721252a74b50d89e6d6459df9aeec4305b3a5a
julienrobert17 <59126582+julienrobert17@users.noreply.github.com>
2022-02-15 15:26:24 +0100

    generation API

--- Generator/Main.java
+++ Generator/Main.java
@@ -13,16 +13,29 @@ public class Main {
 
         String[] templateSplitted = filesTemplate.split("#");
 
-        //GENERATION DES AGENTS
+        //GENERATION DES AGENTS + API
         for(String agent : agents){
             try {
                 List<String> agentInfos = Files.readAllLines(Path.of("./ressources/" + agent + ".txt"));
 
                 PrintWriter writerAgent = new PrintWriter( "./website/" + agent + ".html", "UTF-8");
+                PrintWriter writerAgentApi = new PrintWriter( "./api/get" + agent + ".txt", "UTF-8");
 
+                String AgentJson = "{\"prenom\": \"";
                 String fiche = "";
-
+                
                 fiche = fiche + templateSplitted[0];
+
+                AgentJson += agentInfos.get(0);
+                AgentJson += "\",\"nom\": \"";
+                AgentJson += agentInfos.get(1);
+                AgentJson += "\",\"role\": \"";
+                AgentJson += agentInfos.get(2);
+                AgentJson += "\",\"mdp\": \"";
+                AgentJson += agentInfos.get(3);
+                AgentJson += "\",\"objets\": [";
+
+
 
                 fiche = fiche + agentInfos.get(0) + " " + agentInfos.get(1);
 
@@ -35,6 +48,9 @@ public class Main {
                 int count = 0;
                 for(String objet : agentInfos) {
                     if(count >= 5) {
+                        AgentJson += "\"" + objet;
+                        AgentJson += "\", ";
+
                         fiche = fiche + templateSplitted[3];
                         fiche = fiche + objet;
                         fiche = fiche + templateSplitted[4];
@@ -42,11 +58,17 @@ public class Main {
                     count ++;
                 }
 
+                AgentJson = AgentJson.substring(0, AgentJson.length() -2);
+
+                AgentJson += "]}";
+
                 fiche = fiche + templateSplitted[5];
 
                 writerAgent.println(fiche);
+                writerAgentApi.println(AgentJson);
 
                 writerAgent.close();
+                writerAgentApi.close();
 
             }catch(Exception e) {
                 System.out.println("Aucun fichier " + agent + " trouve");
@@ -64,6 +86,7 @@ public class Main {
 
         fiche = fiche + indexTemplateSplitted[0];
 
+
         for(String agent : agents) {
             fiche = fiche + indexTemplateSplitted[1];
 
@@ -74,13 +97,24 @@ public class Main {
             fiche = fiche + agent;
 
             fiche = fiche + indexTemplateSplitted[3];
+
         }
+
+
 
         fiche = fiche + indexTemplateSplitted[4];
 
         writerIndex.println(fiche);
 
         writerIndex.close();
+    }
 
+
+    public class Agent {
+        public String Nom;
+
+        public String Prenom;
+
+        public List<String> Objets;
     }
 }
